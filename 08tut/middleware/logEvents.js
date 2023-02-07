bfa33f6030dd9a9130bc0db0ecfa298b20af4451
OpenTech-Consult <opentech.consult@gmail.com>
2023-02-07 23:17:20 +0000

    Node.js beginner course

---
+++ 08tut/middleware/logEvents.js
@@ -0,0 +1,28 @@
+const fs = require('node:fs');
+const fsPromise = require('node:fs/promises');
+const path = require('path');
+
+const { format } = require('date-fns');
+const { v4: uuidv4 } = require('uuid');
+
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuidv4()}\t${message}\n`;
+    console.log(logItem);
+    try {
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromise.mkdir(path.join(__dirname, '..', 'logs'));
+        }
+        await fsPromise.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+    console.log(`${req.method}\t${req.path}`);
+    next();
+}
+
+module.exports = { logger, logEvents };
