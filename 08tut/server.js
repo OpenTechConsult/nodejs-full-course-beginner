22491f8d0dde3472491f974bae3df7eba0f63c9f
OpenTech-Consult <opentech.consult@gmail.com>
2023-02-08 23:09:26 +0000

    Node.js Full Course for Beginners
    tutorial 8: how to do routing

--- 08tut/server.js
+++ 08tut/server.js
@@ -41,40 +41,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+// use the routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
-app.get('/new-page(.html)?' , (req , res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?' , (req, res) => {
-    res.redirect(301, '/new-page.html');
-});
-
-// route handlers
-app.get('/hello(.html)?' , (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next();
-}, (req, res) => {
-    res.send('Hello World!');
-});
+app.use(errorHandler);
 
 app.all('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'));
     } else if (req.accepts('json')) {
-        res.json({ error: '404'});
+        res.json({ "error": "404 Not Found" });
     } else {
-        res.type('txt').send('404 not found');
+        res.type('txt').send("404 Not Found");
     }
 });
 
-app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
